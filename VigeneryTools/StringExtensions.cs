27bb210776cb6655a5628424bd58a0b3508501a5
Alexandr <alexandr.psk@gmail.com>
2018-07-14 01:23:51 +0300

    Add Cut\Merge method for string

---
+++ VigeneryTools/StringExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace VigeneryTools
+{
+    static class StringExtensions
+    {
+        public static string[] Cut(this string str, int parts)
+        {
+            if (str == null)
+                throw new ArgumentNullException(nameof(str));
+            if (parts <= 0)
+                throw new ArgumentException("Parts count must be positive number.", nameof(parts));
+
+            string[] result = new string[parts];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                StringBuilder builder = new StringBuilder();
+                for (int j = i; j < str.Length; j += parts)
+                {
+                    builder.Append(str[j]);
+                }
+                result[i] = builder.ToString();
+            }
+
+            return result;
+        }
+
+        public static string Merge(this string[] strings)
+        {
+            var maxLength = strings.Max(s => s.Length);
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                for (int j = 0; j < strings.Length; j++)
+                {
+                    if (strings[j].Length > i)
+                        builder.Append(strings[j][i]);
+                }
+            }
+            return builder.ToString();
+        }
+    }
+}
