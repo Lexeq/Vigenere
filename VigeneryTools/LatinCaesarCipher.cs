9aaac4e8eedbeee0c387108fc7819d062b95faaa
Alexandr <alexandr.psk@gmail.com>
2018-07-28 23:39:47 +0300

    Add LatinCaesarCipher

---
+++ VigeneryTools/LatinCaesarCipher.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System;
+
+namespace VigenereTools
+{
+    internal class LatinCaesarCipher : ICaesarCipher
+    {
+        private const int AlphabetSize = 26;
+
+        public string Decrypt(string text, int shift)
+        {
+            if (text == null)
+                throw new ArgumentNullException(nameof(text));
+
+            return Encrypt(text, -shift);
+        }
+
+        public string Encrypt(string text, int shift)
+        {
+            if (text == null)
+                throw new ArgumentNullException(nameof(text));
+
+            StringBuilder builder = new StringBuilder(text.Length);
+            shift = shift % AlphabetSize;
+
+            foreach (var ch in text)
+            {
+                var x = 'a' + (AlphabetSize + shift + ch - 'a') % AlphabetSize;
+                builder.Append((char)x);
+            }
+            return builder.ToString();
+        }
+    }
+}
