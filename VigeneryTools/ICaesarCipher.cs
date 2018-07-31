1e5ab7aaf527bafd942c8077b85f69540ed96edd
Alexandr <alexandr.psk@gmail.com>
2018-07-31 14:43:31 +0300

    Change ICaesarCipher

--- VigeneryTools/ICaesarCipher.cs
+++ VigeneryTools/ICaesarCipher.cs
@@ -1,7 +1,11 @@
-﻿namespace VigenereTools
+﻿using System.Collections.Generic;
+
+namespace VigenereTools
 {
     public interface ICaesarCipher
     {
+        IReadOnlyCollection<char> Alphabet { get; }
+
         string Encrypt(string text, int shift);
 
         string Decrypt(string text, int shift);
